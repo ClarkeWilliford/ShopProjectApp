7839322daca37fd206357d65c99037bfd3a10ea2
Clarke <clarke578@gmail.com>
2022-04-05 17:23:06 -0400

    created the displayItemView to show the currently chosen item and give the user the option to add to the cart or the wishlist. This view also displays the suggested items.

---
+++ ShopProjectApp/Controllers/SuggestedItemCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SuggestedItemCollectionViewCell.swift
+//  ShopProjectApp
+//
+//  Created by Clarke Williford on 4/5/22.
+//
+
+import UIKit
+
+class SuggestedItemCollectionViewCell: UICollectionViewCell {
+    //Outlet for the Cell
+    @IBOutlet weak var suggestedItemNameLabel: UILabel!
+    @IBOutlet weak var suggestedItemImageView: UIImageView!
+    @IBOutlet weak var suggestedItemPriceLabel: UILabel!
+}
